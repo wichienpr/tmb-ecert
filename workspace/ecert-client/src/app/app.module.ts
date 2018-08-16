9b4a92f82e38a7fa7ec87b8d946e942ddcede153
baiwaDev <pavit@baiwa.co.th>
2018-08-16 16:03:22 +0700

    new project ecert

---
+++ workspace/ecert-client/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { SuiModule } from 'ng2-semantic-ui';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, SuiModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
