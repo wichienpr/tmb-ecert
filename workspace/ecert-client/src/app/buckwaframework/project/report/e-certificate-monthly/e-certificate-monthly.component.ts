0b6cdb3e2eb876f73d492b5308e579508f10ee7a
flmae123flame <flame.tana@gmail.com>
2018-09-04 16:37:45 +0700

    commit

--- workspace/ecert-client/src/app/buckwaframework/project/report/e-certificate-monthly/e-certificate-monthly.component.ts
+++ workspace/ecert-client/src/app/buckwaframework/project/report/e-certificate-monthly/e-certificate-monthly.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {  formatter, TextDateTH } from '../../../common/\u0E49helper/datepicker';
+import { formatter, TextDateTH } from '../../../common/\u0E49helper/datepicker';
 declare var $: any;
 @Component({
   selector: 'app-e-certificate-monthly',
@@ -7,46 +7,41 @@ declare var $: any;
   styleUrls: ['./e-certificate-monthly.component.css']
 })
 export class ECertificateMonthlyComponent implements OnInit {
-  showData: boolean = false; 
+  showData: boolean = false;
   constructor() { }
 
   ngOnInit() {
-    $("#calendar1").calendar({    
+    $("#calendar1").calendar({
       maxDate: new Date(),
-      type: "month",   
-      text: TextDateTH,   
+      type: "month",
+      text: TextDateTH,
       formatter: formatter('month-year')
-       
-  
+
     });
+
+
   }
   ngAfterViewInit() {
-    $("#table").DataTable();
+
   }
-  
   searchData(): void {
     this.showData = true;
     setTimeout(() => {
       $("#table").DataTable({
         scrollX: true,
-        searching :false,
+        searching: false,
+        ordering: false,
+        paging: true,
+
         "columnDefs": [{
-          "targets": 5,
-          "orderable": false
-        },{
-          "targets": 6,
-          "orderable": false
-        },{
-          "targets": 7,
-          "orderable": false
-        },{
-          "targets": 10,
+          "targets": [2,3,4,5,7,8,9],
           "orderable": false
         }]
       });
-    }, 200);
+    });
+
   }
-  
+
   clearData(): void {
     this.showData = false;
   }
