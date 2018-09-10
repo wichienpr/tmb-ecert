1f3b9d42b7b749484c2c340c3b68add1a09c11ce
flmae123flame <flame.tana@gmail.com>
2018-09-11 00:29:51 +0700

    commit

--- workspace/ecert-client/src/app/app.module.ts
+++ workspace/ecert-client/src/app/app.module.ts
@@ -6,11 +6,17 @@ import { LoginComponent } from './buckwaframework/project/pages/login/login.comp
 import { AppRoutingModule } from './buckwaframework/common/configs/app-routing.module';
 import { ComponentsModule } from './buckwaframework/common/components/components.module';
 import { FormsModule } from '@angular/forms';
+import { RoleManagementComponent } from './buckwaframework/project/setup/role-management/role-management.component';
+import { ParameterConfigurationComponent } from './buckwaframework/project/setup/parameter-configuration/parameter-configuration.component';
+import { MonitoringComponent } from './buckwaframework/project/batch-Monitoring/monitoring/monitoring.component';
+import { AuditLogComponent } from './buckwaframework/project/audit-Log/audit-log/audit-log.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+  
+    
   ],
   imports: [
     BrowserModule,
