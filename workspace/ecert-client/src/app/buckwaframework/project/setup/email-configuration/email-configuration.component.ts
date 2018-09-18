dc1898798761431b37ca9fb3b20f7ac821381e8d
flmae123flame <flame.tana@gmail.com>
2018-09-18 16:43:46 +0700

    commit mockup

--- workspace/ecert-client/src/app/buckwaframework/project/setup/email-configuration/email-configuration.component.ts
+++ workspace/ecert-client/src/app/buckwaframework/project/setup/email-configuration/email-configuration.component.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-email-configuration',
   templateUrl: './email-configuration.component.html',
   styleUrls: ['./email-configuration.component.css']
 })
 export class EmailConfigurationComponent implements OnInit {
+  showData: boolean = false;  
 
-  constructor() { }
+  action: string[];
+  selectaction: string;
 
-  ngOnInit() {
+   modal: string[] = ['uploadfile'];
+  constructor() { 
+     this.action = [
+    
+      "All",
+      "Active",
+      "Inactive"
+     
+  ];
+  ;
+
+}
+  ngOnInit() {   
+
+  }
+
+
+  ngAfterViewInit() {
+
+  }
+  searchData(): void {
+    this.showData = true;
+    setTimeout(() => {
+      $("#table").DataTable({
+        scrollX: true,
+        searching: false,
+        orderable : false,             
+        "columnDefs": [{
+          "targets": [3],
+          "orderable": false
+        }]
+      });
+    });
+
+  }
+
+  clearData(): void {
+    this.showData = false;
+  }
+
+  Onselectaction = event => {
+    this.selectaction = this.action[event.target.value];
+  };
+  
+  openModal(id) {
+    $(`#${id}`).modal('show');
+  }
+
+  closeModal(id) {
+    $(`#${id}`).modal('hide');
   }
 
 }
