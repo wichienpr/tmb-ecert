dfab10572923707e07a1ffd4ebb70693826be34b
toffeeDev <saknarin.top21@gmail.com>
2018-09-26 15:45:05 +0700

    create crs00000

---
+++ workspace/front-end/angular-app/src/app/tmb-ecert/crs00000/crs01000/crs01000.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crs01000',
+  templateUrl: './crs01000.component.html',
+  styleUrls: ['./crs01000.component.css']
+})
+export class Crs01000Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
