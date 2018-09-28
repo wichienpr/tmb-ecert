3540a4b914fcf9f0642b368c558f04523ec05d23
toffeeDev <saknarin.top21@gmail.com>
2018-09-28 15:20:10 +0700

    create entity Certificate,ListOfValue,RequestCertificate,RequestForm;

---
+++ workspace/back-end/ecert-webapp/src/main/java/com/tmb/ecert/persistence/entity/ListOfValue.java
@@ -0,0 +1,59 @@
+package com.tmb.ecert.persistence.entity;
+
+public class ListOfValue {
+	private Long listOfValueId;
+	private String code;
+	private Integer type;
+	private String typeDesc;
+	private String name;
+	private Integer sequence;
+
+	public Long getListOfValueId() {
+		return listOfValueId;
+	}
+
+	public void setListOfValueId(Long listOfValueId) {
+		this.listOfValueId = listOfValueId;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public Integer getType() {
+		return type;
+	}
+
+	public void setType(Integer type) {
+		this.type = type;
+	}
+
+	public String getTypeDesc() {
+		return typeDesc;
+	}
+
+	public void setTypeDesc(String typeDesc) {
+		this.typeDesc = typeDesc;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getSequence() {
+		return sequence;
+	}
+
+	public void setSequence(Integer sequence) {
+		this.sequence = sequence;
+	}
+
+}
---
+++ workspace/back-end/ecert-webapp/src/main/java/com/tmb/ecert/persistence/entity/RequestCertificate.java
@@ -0,0 +1,98 @@
+package com.tmb.ecert.persistence.entity;
+
+import java.util.Date;
+
+public class RequestCertificate {
+	private Long reqCertificateId;
+	private Long reqFormId;
+	private String certificateCode;
+	private Integer totalNumber;
+
+	private String createdById;
+	private String createdByName;
+	private Date createdDateTime;
+	private String updateById;
+	private String updateByName;
+	private Date updateDateTime;
+
+	public Long getReqCertificateId() {
+		return reqCertificateId;
+	}
+
+	public void setReqCertificateId(Long reqCertificateId) {
+		this.reqCertificateId = reqCertificateId;
+	}
+
+	public Long getReqFormId() {
+		return reqFormId;
+	}
+
+	public void setReqFormId(Long reqFormId) {
+		this.reqFormId = reqFormId;
+	}
+
+	public String getCertificateCode() {
+		return certificateCode;
+	}
+
+	public void setCertificateCode(String certificateCode) {
+		this.certificateCode = certificateCode;
+	}
+
+	public Integer getTotalNumber() {
+		return totalNumber;
+	}
+
+	public void setTotalNumber(Integer totalNumber) {
+		this.totalNumber = totalNumber;
+	}
+
+	public String getCreatedById() {
+		return createdById;
+	}
+
+	public void setCreatedById(String createdById) {
+		this.createdById = createdById;
+	}
+
+	public String getCreatedByName() {
+		return createdByName;
+	}
+
+	public void setCreatedByName(String createdByName) {
+		this.createdByName = createdByName;
+	}
+
+	public Date getCreatedDateTime() {
+		return createdDateTime;
+	}
+
+	public void setCreatedDateTime(Date createdDateTime) {
+		this.createdDateTime = createdDateTime;
+	}
+
+	public String getUpdateById() {
+		return updateById;
+	}
+
+	public void setUpdateById(String updateById) {
+		this.updateById = updateById;
+	}
+
+	public String getUpdateByName() {
+		return updateByName;
+	}
+
+	public void setUpdateByName(String updateByName) {
+		this.updateByName = updateByName;
+	}
+
+	public Date getUpdateDateTime() {
+		return updateDateTime;
+	}
+
+	public void setUpdateDateTime(Date updateDateTime) {
+		this.updateDateTime = updateDateTime;
+	}
+
+}
