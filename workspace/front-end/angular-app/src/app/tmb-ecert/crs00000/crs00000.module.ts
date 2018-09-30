c5c7ba926f9f9e3ffe1f72f08bd2fc946fbd91cc
toffeeDev <saknarin.top21@gmail.com>
2018-09-30 18:32:48 +0700

    update Code

--- workspace/front-end/angular-app/src/app/tmb-ecert/crs00000/crs00000.module.ts
+++ workspace/front-end/angular-app/src/app/tmb-ecert/crs00000/crs00000.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { Crs00000RoutingModule } from './crs00000-routing.module';
 import { Crs01000Component } from './crs01000/crs01000.component';
 
+// Module Components
+import { ModalModule, CalendarModule, DropdownModule } from 'components/';
+
 @NgModule({
   imports: [
     CommonModule,
-    Crs00000RoutingModule
+    FormsModule,
+    // Routing
+    Crs00000RoutingModule,
+    // Components
+    ModalModule,
+    DropdownModule,
+    CalendarModule,
   ],
   declarations: [Crs01000Component]
 })
--- workspace/front-end/angular-app/src/app/tmb-ecert/crs00000/crs01000/crs01000.component.ts
+++ workspace/front-end/angular-app/src/app/tmb-ecert/crs00000/crs01000/crs01000.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { Crs01000Service } from 'app/tmb-ecert/crs00000/crs01000/crs01000.service';
 
+
+
+declare var $: any;
 @Component({
   selector: 'app-crs01000',
   templateUrl: './crs01000.component.html',
-  styleUrls: ['./crs01000.component.css']
+  styleUrls: ['./crs01000.component.css'],
+ 
 })
 export class Crs01000Component implements OnInit {
+  
+  reqDate:string='';
+  toDate:string='';
+  companyName: string='';
+  organizeId:string='';
+  tmbReqNo:string='';
+  status: string ='';
 
-  constructor() { }
+  requestForm
 
-  ngOnInit() {
+
+
+  constructor(private crs01000Service : Crs01000Service) { 
+    
   }
 
+  ngOnInit() {
+    this.crs01000Service.findAll();
+  }
+ 
+  ngAfterViewInit() {
+    $('.ui.sidebar')
+      .sidebar({
+        context: '.ui.grid.pushable'
+      })
+      .sidebar('setting', 'transition', 'push')
+      .sidebar('toggle');
+  }
+
+  onToggle() {
+    $('.ui.sidebar')
+      .sidebar({
+        context: '.ui.grid.pushable'
+      })
+      .sidebar('setting', 'transition', 'push')
+      .sidebar('toggle');
+  }
+
+}
+
+class RequestForm {
+  
 }
