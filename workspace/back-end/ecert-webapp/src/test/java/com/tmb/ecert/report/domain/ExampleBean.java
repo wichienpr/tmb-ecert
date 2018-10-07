ad8f30c691821f99307c6a77012804633a55240e
toffeeDev <saknarin.top21@gmail.com>
2018-10-07 09:25:19 +0700

    add properties to scr/test/resources

---
+++ workspace/back-end/ecert-webapp/src/test/java/com/tmb/ecert/report/domain/ExampleBean.java
@@ -0,0 +1,24 @@
+package com.tmb.ecert.report.domain;
+
+public class ExampleBean {
+	
+	private String seq;
+	private String orgName;
+
+	public String getSeq() {
+		return seq;
+	}
+
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+
+	public String getOrgName() {
+		return orgName;
+	}
+
+	public void setOrgName(String orgName) {
+		this.orgName = orgName;
+	}
+
+}
