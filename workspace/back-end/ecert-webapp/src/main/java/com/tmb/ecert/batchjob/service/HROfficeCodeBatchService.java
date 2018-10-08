f75002a7b0d21b81084ee02263a5cd35db067f6e
HeroBoy <teeratadpi@gmail.com>
2018-10-08 14:49:10 +0700

    Init HROfficeCode Batch

---
+++ workspace/back-end/ecert-webapp/src/main/java/com/tmb/ecert/batchjob/service/HROfficeCodeBatchService.java
@@ -0,0 +1,17 @@
+package com.tmb.ecert.batchjob.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.tmb.ecert.common.constant.ProjectConstant.BACHJOB_LOG_NAME;
+
+@Service
+public class HROfficeCodeBatchService {
+
+	private static final Logger log = LoggerFactory.getLogger(BACHJOB_LOG_NAME.ECERT_HROFFICECODE);
+	
+	public void runBatchJob() {
+		
+	}
+}
