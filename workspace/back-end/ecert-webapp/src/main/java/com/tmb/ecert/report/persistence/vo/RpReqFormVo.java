a271536ec7e81894b763db1dc08da7e737aba88a
toffeeDev <saknarin.top21@gmail.com>
2018-10-10 14:09:30 +0700

    add Bean RpReqFormVo

--- workspace/back-end/ecert-webapp/src/main/java/com/tmb/ecert/report/persistence/vo/RpReqFormVo.java
+++ workspace/back-end/ecert-webapp/src/main/java/com/tmb/ecert/report/persistence/vo/RpReqFormVo.java
@@ -2,6 +2,21 @@ package com.tmb.ecert.report.persistence.vo;
 
 public class RpReqFormVo {
 	private String seq;
+	private String typeCertificate;
+	private String customerName;
+	private String telephone;
+	private String reqDate;
+	private String organizeId;
+	private String companyName;
+	private String accountName;
+	private String totalNumber;
+	private String totalNumberCc;
+	private String registeredDateEdit;
+	private String other;
+	private String registeredDateOther;
+	private String statementYear;
+	private String acceptedDate;
+	private String tmpReqNo;
 
 	public String getSeq() {
 		return seq;
@@ -10,6 +25,125 @@ public class RpReqFormVo {
 	public void setSeq(String seq) {
 		this.seq = seq;
 	}
-	
-	
+
+	public String getTypeCertificate() {
+		return typeCertificate;
+	}
+
+	public void setTypeCertificate(String typeCertificate) {
+		this.typeCertificate = typeCertificate;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getReqDate() {
+		return reqDate;
+	}
+
+	public void setReqDate(String reqDate) {
+		this.reqDate = reqDate;
+	}
+
+	public String getOrganizeId() {
+		return organizeId;
+	}
+
+	public void setOrganizeId(String organizeId) {
+		this.organizeId = organizeId;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getAccountName() {
+		return accountName;
+	}
+
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+
+	public String getTotalNumber() {
+		return totalNumber;
+	}
+
+	public void setTotalNumber(String totalNumber) {
+		this.totalNumber = totalNumber;
+	}
+
+	public String getTotalNumberCc() {
+		return totalNumberCc;
+	}
+
+	public void setTotalNumberCc(String totalNumberCc) {
+		this.totalNumberCc = totalNumberCc;
+	}
+
+	public String getRegisteredDateEdit() {
+		return registeredDateEdit;
+	}
+
+	public void setRegisteredDateEdit(String registeredDateEdit) {
+		this.registeredDateEdit = registeredDateEdit;
+	}
+
+	public String getOther() {
+		return other;
+	}
+
+	public void setOther(String other) {
+		this.other = other;
+	}
+
+	public String getRegisteredDateOther() {
+		return registeredDateOther;
+	}
+
+	public void setRegisteredDateOther(String registeredDateOther) {
+		this.registeredDateOther = registeredDateOther;
+	}
+
+	public String getStatementYear() {
+		return statementYear;
+	}
+
+	public void setStatementYear(String statementYear) {
+		this.statementYear = statementYear;
+	}
+
+	public String getAcceptedDate() {
+		return acceptedDate;
+	}
+
+	public void setAcceptedDate(String acceptedDate) {
+		this.acceptedDate = acceptedDate;
+	}
+
+	public String getTmpReqNo() {
+		return tmpReqNo;
+	}
+
+	public void setTmpReqNo(String tmpReqNo) {
+		this.tmpReqNo = tmpReqNo;
+	}
+
 }
