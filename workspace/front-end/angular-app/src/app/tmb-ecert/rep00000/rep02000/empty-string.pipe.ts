263597200e6fd9a0ab05b4e8630ec5b274f04af9
arm2017 <arm2017@gmail.com>
2018-10-18 15:10:19 +0700

    FIx bug build

--- workspace/front-end/angular-app/src/app/tmb-ecert/rep00000/rep02000/empty-string.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-declare var $: any;
-
-@Pipe({ name: 'emptyString' })
-export class EmptyStringPipe implements PipeTransform {
-  transform(value: String): String {
-    return $.trim(value) == "" ? "-" : value;
-  }
-}
