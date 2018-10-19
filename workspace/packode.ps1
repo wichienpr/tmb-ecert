7ded8b5c4c68782f6591b74ba97a6b18dd721276
arm2017 <arm2017@gmail.com>
2018-10-19 18:22:57 +0700

    update pack code

--- workspace/packode.ps1
+++ workspace/packode.ps1
@@ -1,3 +1,11 @@
+echo "  ____        _                 _____           _     __          __         "
+echo " |  _ \      (_)               |  __ \         | |    \ \        / /         "
+echo " | |_) | __ _ ___      ____ _  | |__) |_ _  ___| | __  \ \  /\  / /_ _ _ __  "
+echo " |  _ < / _* | \ \ /\ / / _* | |  ___/ _* |/ __| |/ /   \ \/  \/ / _* | '__| "
+echo " | |_) | (_| | |\ V  V / (_| | | |  | (_| | (__|   <     \  /\  / (_| | |    "
+echo " |____/ \__,_|_| \_/\_/ \__,_| |_|   \__,_|\___|_|\_\     \/  \/ \__,_|_|    "
+echo "                                                                             "
+
 cd .\front-end\angular-app\
 echo "1. UPDATE LIB"
 npm install
@@ -9,6 +17,8 @@ Remove-Item .\back-end\ecert-webapp\src\main\resources\static\app -Force -Recurs
 Copy-Item .\front-end\angular-app\dist\angular-app -Destination .\back-end\ecert-webapp\src\main\resources\static\app -Recurse
 echo "3. COPY SUCCESS"
 
+echo "3. PACK WAR"
 cd .\back-end\ecert-webapp\
 mvn clean package -DskipTests
 cd ../..
+
