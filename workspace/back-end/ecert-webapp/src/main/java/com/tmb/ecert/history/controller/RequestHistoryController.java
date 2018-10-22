a82801023272eed1748f4450006535111d14e4ac
KimJaeHa <artistkanjai1@gmail.com>
2018-10-22 18:28:47 +0700

    request history datatable

--- workspace/back-end/ecert-webapp/src/main/java/com/tmb/ecert/history/controller/RequestHistoryController.java
+++ workspace/back-end/ecert-webapp/src/main/java/com/tmb/ecert/history/controller/RequestHistoryController.java
@@ -6,11 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.tmb.ecert.history.persistence.vo.RequestHistoryVo;
 import com.tmb.ecert.history.service.RequestHistoryService;
+
+import th.co.baiwa.buckwaframework.common.bean.DataTableResponse;
 
 @RequestMapping("api/history/")
 @Controller
@@ -35,6 +39,12 @@ public class RequestHistoryController {
 	@ResponseBody
 	public List<RequestHistoryVo> list() {
 		return reqHistory.findAll();
+	}
+	
+	@PostMapping("/list")
+	@ResponseBody
+	public DataTableResponse<RequestHistoryVo> listByVo(@RequestBody RequestHistoryVo vo) {
+		return reqHistory.findByVo(vo);
 	}
 	
 	@GetMapping("/list/{id}")
