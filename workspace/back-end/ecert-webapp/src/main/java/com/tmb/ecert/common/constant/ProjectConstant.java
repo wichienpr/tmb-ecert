605acb56a70c8c0aac78add24c2f372b1330f85e
KimJaeHa <artistkanjai1@gmail.com>
2018-10-25 17:12:13 +0700

    add params

--- workspace/back-end/ecert-webapp/src/main/java/com/tmb/ecert/common/constant/ProjectConstant.java
+++ workspace/back-end/ecert-webapp/src/main/java/com/tmb/ecert/common/constant/ProjectConstant.java
@@ -2,23 +2,23 @@ package com.tmb.ecert.common.constant;
 
 public class ProjectConstant {
 
-	public static class APPLICATION_LOG_NAME{
+	public static class APPLICATION_LOG_NAME {
 		public static final String ECERT_REQFORM = "e-certificate-requestform.log";
 		public static final String ECERT_SEARCH_REQFORM = "e-certificate-search-requesform.log";
 		public static final String ECERT_PAYMENT = "e-certificate-payment.log";
 		public static final String ECERT_REPORT = "e-certificate-report.log";
 		public static final String ECERT_BATCHMONITORING = "e-certificate-batchmonitoring.log";
 		public static final String ECERT_AUDITLOG = "e-certificate-auditlog.log";
-		public static final String ECERT_ROLEMANAGEMENT = "e-certificate-rolemangement.log";	
+		public static final String ECERT_ROLEMANAGEMENT = "e-certificate-rolemangement.log";
 		public static final String ECERT_PARAMETERCONFIG = "e-certificate-parameterconfig.log";
 		public static final String ECERT_EMAILCONFIG = "e-certificate-emailconfig.log";
 	}
-		
+
 	public static class CHANNEL {
 		public static final String BATCH = "BATCH";
 	}
-	
-	public static class SYSTEM{
+
+	public static class SYSTEM {
 		public static final String TMB_BANKCODE = "tmb.bankcode";
 		public static final String TMB_TRANSACTION_CODE = "tmb.transactiono";
 		public static final String TMB_SERVICE_CODE = "tmb.servicecode";
@@ -26,12 +26,12 @@ public class ProjectConstant {
 		public static final String DBD_ACCOUNT_NUMBER = "dbd.accountno";
 		public static final String TMB_TRAN_TYPE = "tmb.transaction.type";
 	}
-	
+
 	public static class ENCODING {
 		public static final String TIS_620 = "TIS-620";
 	}
-	
-	public static class ACTION_AUDITLOG{
+
+	public static class ACTION_AUDITLOG {
 		public static final String LOGIN_CODE = "70001";
 		public static final String LOGOUT_CODE = "70002";
 		public static final String REQFORM_01_CODE = "70003";
@@ -51,10 +51,10 @@ public class ProjectConstant {
 		public static final String DOWNLOAD_ECERT_CODE = "70016";
 		public static final String PRINT_FORM_CODE = "70018";
 		public static final String PRINT_FORMREQ_CODE = "70019";
-		
+
 	}
-	
-	public static class ACTION_AUDITLOG_DESC{
+
+	public static class ACTION_AUDITLOG_DESC {
 		public static final String LOGIN = "auditlog.login";
 		public static final String LOGOUT = "auditlog.logout";
 		public static final String REQFORM_01 = "auditlog.reqform.01";
@@ -74,20 +74,43 @@ public class ProjectConstant {
 		public static final String DOWNLOAD_ECERT_CODE = "auditlog.downloadcertificate";
 		public static final String PRINT_FORM = "auditlog.reqform.04";
 		public static final String PRINT_FORMREQ = "auditlog.reqform.05";
-		
+
 	}
-	
-	public static class SERVICE_TIMMING{
+
+	public static class SERVICE_TIMMING {
 		public static final String SHUTDOWN_TIME_TO = "shutdown.time.to";
 		public static final String SHUTDOWN_TIME_FROM = "shutdown.time.from";
 	}
-	
-	public static class WEB_SERVICE_ENDPOINT{
-		public static final String  UPLOADCERTIFICARE_SLEEP= "ecer.upload.certificate.sleep";
-		public static final String  UPLOADCERTIFICARE_TIME= "ecer.upload.certificate.time";
-		public static final String  ECM_IMPORT_DOCUMENT= "ecert.ecm.importdocument.endpoint";
-		public static final String  ECM_CHECK_STATUS = "ecert.ecm.checkstatus.endpoint";
-		public static final String  ECM_CHANNELID = "ecm.channelid";
-		public static final String  ECM_DOCTYPE = "ecm.doctype";
+
+	public static class WEB_SERVICE_ENDPOINT {
+		public static final String UPLOADCERTIFICARE_SLEEP = "ecer.upload.certificate.sleep";
+		public static final String UPLOADCERTIFICARE_TIME = "ecer.upload.certificate.time";
+		public static final String ECM_IMPORT_DOCUMENT = "ecert.ecm.importdocument.endpoint";
+		public static final String ECM_CHECK_STATUS = "ecert.ecm.checkstatus.endpoint";
+		public static final String ECM_CHANNELID = "ecm.channelid";
+		public static final String ECM_DOCTYPE = "ecm.doctype";
+		public static final String XPRESS_BILLPAYMENT = "ecert.xpress.billpayment.endpoint";
+		public static final String APPROVE_BEFOREPAYMENT = "ecert.dbd.approvebeforepay.endpoint";
+		public static final String REALTIME_PAYMENT = "ecert.dbd.realtimepayment.endpoint";
+	}
+
+	public static class WEB_SERVICE_PARAMS {
+		public static final String TMB_BANKCODE = "tmb.bankcode";
+		public static final String TMB_SERVICECODE = "tmb.servicecode";
+		public static final String TMB_TRANSACTION_NO = "tmb.transactiono";
+		public static final String TMB_PAYMENT_TYPE = "tmb.payment.type";
+		
+		public static final String FEE_CUST_LANG = "feepayment.customer.language";
+		public static final String FEE_CLIENT_ORG = "feepayment.clientapp.org";
+		public static final String FEE_CLIENT_NAME = "feepayment.clientapp.name";
+		public static final String FEE_CLIENT_VERSION = "feepayment.clientapp.version";
+		public static final String FEE_SPNAME = "feepayment.SPName";
+		public static final String FEE_TRANS_CODE = "feepayment.transaction.code";
+		public static final String FEE_FROM_ACCOUNT_TYPE = "feepayment.fromacctref.account.type";
+		public static final String DBD_ACCOUNT = "dbd.accountno";
+		public static final String FEE_TO_ACCOUNT_TYPE = "feepayment.toacctref.account.type";
+		public static final String FEE_EPAY = "feepayment.epay.code";
+		public static final String FEE_BRANCH = "feepayment.branchIdent";
+		public static final String FEE_COMP_CODE = "feepayment.compcode";
 	}
 }
--- workspace/back-end/ecert-webapp/src/main/java/com/tmb/ecert/common/service/PaymentWebService.java
+++ workspace/back-end/ecert-webapp/src/main/java/com/tmb/ecert/common/service/PaymentWebService.java
@@ -21,7 +21,8 @@ import com.tmb.ecert.checkrequeststatus.persistence.vo.ws.RealtimePaymentRequest
 import com.tmb.ecert.checkrequeststatus.persistence.vo.ws.RealtimePaymentResponse;
 import com.tmb.ecert.common.constant.DateConstant;
 import com.tmb.ecert.common.constant.ProjectConstant.APPLICATION_LOG_NAME;
-import com.tmb.ecert.common.constant.StatusConstant;
+import com.tmb.ecert.common.constant.ProjectConstant.WEB_SERVICE_ENDPOINT;
+import com.tmb.ecert.common.constant.ProjectConstant.WEB_SERVICE_PARAMS;
 import com.tmb.ecert.common.constant.StatusConstant.PAYMENT_STATUS;
 import com.tmb.ecert.common.domain.CommonMessage;
 import com.tmb.ecert.common.domain.RequestForm;
@@ -44,31 +45,31 @@ public class PaymentWebService {
 		FeePaymentRequest req = new FeePaymentRequest();
 		req.setRqUid(uuid);
 		req.setClientDt(DateConstant.convertDateToStrDDMMYYYYHHmmss(new Date()));
-		req.setCustLangPref(ApplicationCache.getParamValueByName("feepayment.customer.language"));
-		req.setClientAppOrg(ApplicationCache.getParamValueByName("feepayment.clientapp.org"));
-		req.setClientAppName(ApplicationCache.getParamValueByName("feepayment.clientapp.name"));
-		req.setClientAppVersion(ApplicationCache.getParamValueByName("feepayment.clientapp.version"));
-		req.setSpName(ApplicationCache.getParamValueByName("feepayment.SPName"));
-		req.setTranCode(ApplicationCache.getParamValueByName("feepayment.transaction.code"));
+		req.setCustLangPref(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.FEE_CUST_LANG));
+		req.setClientAppOrg(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.FEE_CLIENT_ORG));
+		req.setClientAppName(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.FEE_CLIENT_NAME));
+		req.setClientAppVersion(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.FEE_CLIENT_VERSION));
+		req.setSpName(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.FEE_SPNAME));
+		req.setTranCode(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.FEE_TRANS_CODE));
 		req.setFromAccountIdent(reqF.getAccountNo()); // ECERT_REQUEST_FORM.ACCOUNT_NO
 		// ECERT_REQUEST_FORM. ACCOUNTTYPE
 		req.setFromAccountType(reqF.getAccountType() != null ? reqF.getAccountType()
-				: ApplicationCache.getParamValueByName("feepayment.fromacctref.account.type"));
-		req.setToAccountIdent(ApplicationCache.getParamValueByName("dbd.accountno"));
-		req.setToAccountType(ApplicationCache.getParamValueByName("feepayment.toacctref.account.type"));
+				: ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.FEE_FROM_ACCOUNT_TYPE));
+		req.setToAccountIdent(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.DBD_ACCOUNT));
+		req.setToAccountType(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.FEE_TO_ACCOUNT_TYPE));
 		req.setCurAmt(new BigDecimal(0.0)); // ECERT_REQUEST_FORM.AMOUNT_DBD
 		req.setBillPmtFee(reqF.getAmountTmb()); // ECERT_REQUEST_FORM.AMOUNT_TMB
 		req.setRef1(reqF.getRef1()); // ECERT_REQUEST_FORM.REF1
 		req.setRef2(reqF.getRef2()); // ECERT_REQUEST_FORM.REF2
 		req.setPostedDate(DateConstant.convertDateToStrDDMMYYYY(new Date()));
-		req.setEpayCode(ApplicationCache.getParamValueByName("feepayment.epay.code"));
-		req.setBranchIdent(ApplicationCache.getParamValueByName("feepayment.branchIdent"));
-		req.setCompCode(ApplicationCache.getParamValueByName("feepayment.compcode"));
+		req.setEpayCode(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.FEE_EPAY));
+		req.setBranchIdent(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.FEE_BRANCH));
+		req.setCompCode(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.FEE_COMP_CODE));
 		req.setPostedTime(DateConstant.convertDateToStrHHmmss(new Date()));
 		try {
 			RestTemplate restTemplate = new RestTemplate();
 			HttpEntity<FeePaymentRequest> request = new HttpEntity<>(req);
-			ResponseEntity<FeePaymentResponse> response = restTemplate.exchange(WS_URL + "api-payment/add",
+			ResponseEntity<FeePaymentResponse> response = restTemplate.exchange(ApplicationCache.getParamValueByName(WEB_SERVICE_ENDPOINT.XPRESS_BILLPAYMENT),
 					HttpMethod.POST, request, FeePaymentResponse.class);
 			FeePaymentResponse res = response.getBody();
 			if (PAYMENT_STATUS.SUCCESS.equalsIgnoreCase(res.getStatusCode())) {
@@ -94,31 +95,31 @@ public class PaymentWebService {
 		FeePaymentRequest req = new FeePaymentRequest();
 		req.setRqUid(uuid);
 		req.setClientDt(DateConstant.convertDateToStrDDMMYYYYHHmmss(new Date()));
-		req.setCustLangPref(ApplicationCache.getParamValueByName("feepayment.customer.language"));
-		req.setClientAppOrg(ApplicationCache.getParamValueByName("feepayment.clientapp.org"));
-		req.setClientAppName(ApplicationCache.getParamValueByName("feepayment.clientapp.name"));
-		req.setClientAppVersion(ApplicationCache.getParamValueByName("feepayment.clientapp.version"));
-		req.setSpName(ApplicationCache.getParamValueByName("feepayment.SPName"));
-		req.setTranCode(ApplicationCache.getParamValueByName("feepayment.transaction.code"));
+		req.setCustLangPref(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.FEE_CUST_LANG));
+		req.setClientAppOrg(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.FEE_CLIENT_ORG));
+		req.setClientAppName(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.FEE_CLIENT_NAME));
+		req.setClientAppVersion(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.FEE_CLIENT_VERSION));
+		req.setSpName(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.FEE_SPNAME));
+		req.setTranCode(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.FEE_TRANS_CODE));
 		req.setFromAccountIdent(reqF.getAccountNo()); // ECERT_REQUEST_FORM.ACCOUNT_NO
 		// ECERT_REQUEST_FORM. ACCOUNTTYPE
 		req.setFromAccountType(reqF.getAccountType() != null ? reqF.getAccountType()
-				: ApplicationCache.getParamValueByName("feepayment.fromacctref.account.type"));
-		req.setToAccountIdent(ApplicationCache.getParamValueByName("dbd.accountno"));
-		req.setToAccountType(ApplicationCache.getParamValueByName("feepayment.toacctref.account.type"));
+				: ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.FEE_FROM_ACCOUNT_TYPE));
+		req.setToAccountIdent(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.DBD_ACCOUNT));
+		req.setToAccountType(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.FEE_TO_ACCOUNT_TYPE));
 		req.setCurAmt(reqF.getAmountDbd()); // ECERT_REQUEST_FORM.AMOUNT_DBD
 		req.setBillPmtFee(new BigDecimal(0.0)); // ECERT_REQUEST_FORM.AMOUNT_TMB
 		req.setRef1(reqF.getRef1()); // ECERT_REQUEST_FORM.REF1
 		req.setRef2(reqF.getRef2()); // ECERT_REQUEST_FORM.REF2
 		req.setPostedDate(DateConstant.convertDateToStrDDMMYYYY(new Date()));
-		req.setEpayCode(ApplicationCache.getParamValueByName("feepayment.epay.code"));
-		req.setBranchIdent(ApplicationCache.getParamValueByName("feepayment.branchIdent"));
-		req.setCompCode(ApplicationCache.getParamValueByName("feepayment.compcode"));
+		req.setEpayCode(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.FEE_EPAY));
+		req.setBranchIdent(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.FEE_BRANCH));
+		req.setCompCode(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.FEE_COMP_CODE));
 		req.setPostedTime(DateConstant.convertDateToStrHHmmss(new Date()));
 		try {
 			RestTemplate restTemplate = new RestTemplate();
 			HttpEntity<FeePaymentRequest> request = new HttpEntity<>(req);
-			ResponseEntity<FeePaymentResponse> response = restTemplate.exchange(WS_URL + "api-payment/add",
+			ResponseEntity<FeePaymentResponse> response = restTemplate.exchange(ApplicationCache.getParamValueByName(WEB_SERVICE_ENDPOINT.XPRESS_BILLPAYMENT),
 					HttpMethod.POST, request, FeePaymentResponse.class);
 			FeePaymentResponse res = response.getBody();
 			if (PAYMENT_STATUS.SUCCESS.equalsIgnoreCase(res.getStatusCode())) {
@@ -140,8 +141,8 @@ public class PaymentWebService {
 		logger.info("PaymentWebService::approveBeforePayment");
 		CommonMessage<ApproveBeforePayResponse> commonMsg = new CommonMessage<ApproveBeforePayResponse>();
 		ApproveBeforePayRequest req = new ApproveBeforePayRequest();
-		req.setBankCode(ApplicationCache.getParamValueByName("tmb.bankcode"));
-		req.setServiceCode(ApplicationCache.getParamValueByName("tmb.servicecode"));
+		req.setBankCode(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.TMB_BANKCODE));
+		req.setServiceCode(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.TMB_SERVICECODE));
 		req.setRef1(reqF.getRef1());
 		req.setRef2(reqF.getRef2());
 		req.setAmount(reqF.getAmountDbd());
@@ -149,7 +150,7 @@ public class PaymentWebService {
 			RestTemplate restTemplate = new RestTemplate();
 			HttpEntity<ApproveBeforePayRequest> request = new HttpEntity<>(req);
 			ResponseEntity<ApproveBeforePayResponse> response = restTemplate.exchange(
-					WS_URL + "api-payment/approveBeforePay", HttpMethod.POST, request, ApproveBeforePayResponse.class);
+					ApplicationCache.getParamValueByName(WEB_SERVICE_ENDPOINT.APPROVE_BEFOREPAYMENT), HttpMethod.POST, request, ApproveBeforePayResponse.class);
 			ApproveBeforePayResponse res = response.getBody();
 			if (PAYMENT_STATUS.SUCCESS.equalsIgnoreCase(res.getStatusCode())) {
 				commonMsg.setData(res);
@@ -170,13 +171,13 @@ public class PaymentWebService {
 		logger.info("PaymentWebService::realtimePayment");
 		CommonMessage<RealtimePaymentResponse> commonMsg = new CommonMessage<RealtimePaymentResponse>();
 		RealtimePaymentRequest req = new RealtimePaymentRequest();
-		req.setServiceCode(ApplicationCache.getParamValueByName("tmb.servicecode"));
-		req.setTransactionNo(ApplicationCache.getParamValueByName("tmb.transactiono"));
+		req.setServiceCode(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.TMB_SERVICECODE));
+		req.setTransactionNo(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.TMB_TRANSACTION_NO));
 		req.setRef1(reqF.getRef1());
 		req.setRef2(reqF.getRef2());
-		req.setBankCode(ApplicationCache.getParamValueByName("tmb.bankcode"));
+		req.setBankCode(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.TMB_BANKCODE));
 		req.setBranchCode(reqF.getPaymentBranchCode());
-		req.setPaymentType(ApplicationCache.getParamValueByName("tmb.payment.type"));
+		req.setPaymentType(ApplicationCache.getParamValueByName(WEB_SERVICE_PARAMS.TMB_PAYMENT_TYPE));
 		req.setPayAmount(reqF.getAmountDbd());
 		req.setPaymentDate(DateConstant.convertDateToStrDDMMYYYYHHmmss(reqF.getPaymentDate()));
 		req.setPaymentName(reqF.getCustomerName());
@@ -186,7 +187,7 @@ public class PaymentWebService {
 			RestTemplate restTemplate = new RestTemplate();
 			HttpEntity<RealtimePaymentRequest> request = new HttpEntity<>(req);
 			ResponseEntity<RealtimePaymentResponse> response = restTemplate.exchange(
-					WS_URL + "api-payment/realTimePayment", HttpMethod.POST, request, RealtimePaymentResponse.class);
+					ApplicationCache.getParamValueByName(WEB_SERVICE_ENDPOINT.REALTIME_PAYMENT), HttpMethod.POST, request, RealtimePaymentResponse.class);
 			RealtimePaymentResponse res = response.getBody();
 			if (PAYMENT_STATUS.SUCCESS.equalsIgnoreCase(res.getStatusCode())) {
 				commonMsg.setData(res);
