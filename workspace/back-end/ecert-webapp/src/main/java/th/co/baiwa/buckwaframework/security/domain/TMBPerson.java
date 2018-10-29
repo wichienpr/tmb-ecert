d9f6268a6158a8203059c5b5ba6cf0648a04e95c
arm2017 <arm2017@gmail.com>
2018-10-29 15:42:16 +0700

    add AD , and deploy jboss

---
+++ workspace/back-end/ecert-webapp/src/main/java/th/co/baiwa/buckwaframework/security/domain/TMBPerson.java
@@ -0,0 +1,79 @@
+package th.co.baiwa.buckwaframework.security.domain;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class TMBPerson implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String useranme;
+    private String password;
+    private String tmbcn;
+    private String userid;
+    private String name;
+    private String group;
+    private List<String> memberOfs;//memberO
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public List<String> getMemberOfs() {
+        return memberOfs;
+    }
+
+    public void setMemberOfs(List<String> memberOfs) {
+        this.memberOfs = memberOfs;
+    }
+
+    public String getTmbcn() {
+        return tmbcn;
+    }
+
+    public void setTmbcn(String tmbcn) {
+        this.tmbcn = tmbcn;
+    }
+
+    public String getUseranme() {
+        return useranme;
+    }
+
+    public void setUseranme(String useranme) {
+        this.useranme = useranme;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+	@Override
+	public String toString() {
+		return "TMBPerson [useranme=" + useranme + ", password=" + password + ", tmbcn=" + tmbcn + ", userid=" + userid
+				+ ", name=" + name + ", group=" + group + ", memberOfs=" + memberOfs + "]";
+	}
+    
+    
+}
---
+++ workspace/back-end/ecert-webapp/src/main/java/th/co/baiwa/buckwaframework/security/provider/TmbAuthenticationProvider.java
@@ -0,0 +1,89 @@
+package th.co.baiwa.buckwaframework.security.provider;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.NullAuthoritiesMapper;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.Assert;
+
+import th.co.baiwa.buckwaframework.security.domain.TMBPerson;
+import th.co.baiwa.buckwaframework.security.service.UserDetailsService;
+
+public class TmbAuthenticationProvider  implements AuthenticationProvider {
+
+	private static final Logger logger = LoggerFactory.getLogger(TmbAuthenticationProvider.class);
+	private GrantedAuthoritiesMapper authoritiesMapper = new NullAuthoritiesMapper();
+
+	private UserDetailsService userDetailsService;
+
+
+//	@Value("${ldap.useLdap}")
+//	private String useLdap;
+
+	@Autowired
+	private TMBLDAPManager tmbldapManager;
+
+
+
+//	@Autowired
+//	@Qualifier("passwordEncoder")
+//	@Override
+//	public void setPasswordEncoder(Object passwordEncoder) {
+//		if (FLAG.N_FLAG.equals(useLdap)) {
+//			super.setPasswordEncoder(passwordEncoder);
+//		}
+//	}
+	
+	
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		logger.info("authenticate");
+		Assert.notNull(userDetailsService, "userDetailsService is null : [TmbAuthenticationProvider]");
+		String username = authentication.getPrincipal().toString();
+		String password = authentication.getCredentials().toString();
+		
+		UserDetails user = null;
+
+		try {
+			TMBPerson tmb = tmbldapManager.isAuthenticate(username, password);
+			logger.info("AD is OK. user :{} ,  userid : {} " , username, tmb.getUserid());
+			user = userDetailsService.loadUserByUsername(username, tmb);
+			
+		} catch (Exception e) {
+			throw new BadCredentialsException(e.getMessage());
+		}
+		
+		Assert.notNull(user, "UserDetails is null : [TmbAuthenticationProvider]");
+		return createSuccessAuthentication(user,authentication,user);
+				
+	}
+	
+	protected Authentication createSuccessAuthentication(Object principal,	Authentication authentication, UserDetails user) {
+		UsernamePasswordAuthenticationToken result = new UsernamePasswordAuthenticationToken(
+				principal, authentication.getCredentials(),
+				authoritiesMapper.mapAuthorities(user.getAuthorities()));
+		result.setDetails(authentication.getDetails());
+
+		return result;
+	}
+
+	@Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(
+          UsernamePasswordAuthenticationToken.class);
+    }
+	
+
+	public void setUserDetailsService(UserDetailsService userDetailsService) {
+		this.userDetailsService = userDetailsService;
+	}
+	
+	
+}
