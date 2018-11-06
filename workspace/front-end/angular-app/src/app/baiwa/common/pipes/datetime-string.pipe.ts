622c85039459497510019bc8c3cd6bc521eeebf7
KimJaeHa <artistkanjai1@gmail.com>
2018-11-06 15:20:07 +0700

    datetime IE 11 supported

--- workspace/front-end/angular-app/src/app/baiwa/common/pipes/datetime-string.pipe.ts
+++ workspace/front-end/angular-app/src/app/baiwa/common/pipes/datetime-string.pipe.ts
@@ -1,15 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { digit } from 'helpers/';
+import * as moment from 'moment';
 
 @Pipe({ name: 'datetimeString' })
 export class DatetimeStringPipe implements PipeTransform {
     transform(value: Date, local: string = "en"): string {
-        const date = new Date(value);
-        const _dd = digit(date.getDate());
-        const _MM = digit(date.getMonth()+1);
-        const _yyyy = digit(date.getFullYear());
-        const _HH = digit(date.getHours());
-        const _mm = digit(date.getMinutes());
-        return `${_dd}/${_MM}/${_yyyy} ${_HH}:${_mm}`;
+        // const date = new Date(value);
+        // const _dd = digit(date.getDate());
+        // const _MM = digit(date.getMonth()+1);
+        // const _yyyy = digit(date.getFullYear());
+        // const _HH = digit(date.getHours());
+        // const _mm = digit(date.getMinutes());
+        // return `${_dd}/${_MM}/${_yyyy} ${_HH}:${_mm}`;
+        return moment(value).format('DD/MM/YYYY HH:mm');
     }
 }
