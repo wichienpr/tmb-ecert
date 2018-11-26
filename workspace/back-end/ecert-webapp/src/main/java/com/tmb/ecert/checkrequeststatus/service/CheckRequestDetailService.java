fb938fd8a2e30b9a8a9dd3cc7f7d051d763b412a
KimJaeHa <artistkanjai1@gmail.com>
2018-11-26 17:19:26 +0700

    user officeCode

--- workspace/back-end/ecert-webapp/src/main/java/com/tmb/ecert/checkrequeststatus/service/CheckRequestDetailService.java
+++ workspace/back-end/ecert-webapp/src/main/java/com/tmb/ecert/checkrequeststatus/service/CheckRequestDetailService.java
@@ -163,6 +163,7 @@ public class CheckRequestDetailService {
 		RequestForm newReq = new RequestForm();
 		try {
 			newReq = dao.findReqFormById(id, false);
+			newReq.setOfficeCode(user.getOfficeCode());
 			logger.info("CheckRequestDetailService::approve PAYMENT_TYPE => {}", newReq.getPaidTypeCode());
 			if (UserLoginUtils.ishasRoleName(user, ADConstant.ROLE_CHECKER)) {
 				// UserLoginUtils.ishasRole(user, ROLES.CHECKER)
