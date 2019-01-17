facc4d9153cb7fe67f75bdb57a2b5df0b44e1339
keadtisak <chaiyawanlive@gmail.com>
2019-01-17 13:47:31 +0700

    fix bug video

--- workspace/back-end/ecert-webapp/src/main/java/com/tmb/ecert/common/controller/ManualController.java
+++ workspace/back-end/ecert-webapp/src/main/java/com/tmb/ecert/common/controller/ManualController.java
@@ -1,17 +1,21 @@
 package com.tmb.ecert.common.controller;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.compress.utils.IOUtils;
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +26,7 @@ import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBo
 import com.tmb.ecert.common.constant.ProjectConstant;
 import com.tmb.ecert.common.service.DownloadService;
 
+import th.co.baiwa.buckwaframework.common.util.EcertFileUtils;
 import th.co.baiwa.buckwaframework.support.ApplicationCache;
 
 @RequestMapping("api/manual")
@@ -45,6 +50,40 @@ public class ManualController {
 		return new FileSystemResource(new File(pathFile));
 		
 	}
+	
+	@GetMapping("/videoDownload")
+	@ResponseBody
+	public  void getfile(HttpServletResponse response) {
+		String pathFile = ApplicationCache.getParamValueByName(ProjectConstant.ECERT_MANUAL.MANUAL_PATHVIDEO);
+		ByteArrayOutputStream outByteStream = null;
+		OutputStream outStream = null;
+		FileInputStream file = null;
+		byte[] data = null;
+		try {
+			file = new FileInputStream(new File(pathFile));
+			data = IOUtils.toByteArray(file);
+			
+			outByteStream = new ByteArrayOutputStream();
+			outByteStream.write(data);
+			byte[] outArray = outByteStream.toByteArray();
+			response.setContentType("video/mp4");
+			response.setContentLength(outArray.length);
+			response.setHeader("Expires:", "0"); // eliminates browser caching
+//			response.setHeader("Content-Disposition", "attachment; filename=" +"eee"  + ".xlsx");
+			outStream = response.getOutputStream();
+			outStream.write(outArray);
+			outStream.flush();
+			
+		}catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+			EcertFileUtils.closeStream(outByteStream);
+			EcertFileUtils.closeStream(outStream);
+		}
+	}
+	
+
 
 
 }
