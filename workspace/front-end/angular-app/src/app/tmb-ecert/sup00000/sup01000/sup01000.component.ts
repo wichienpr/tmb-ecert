1276b7c25d1fd7e7fc10e16aa9579e80d8e4c5a3
keadtisak <chaiyawanlive@gmail.com>
2019-01-31 15:47:34 +0700

    fix security issue

--- workspace/front-end/angular-app/src/app/tmb-ecert/sup00000/sup01000/sup01000.component.ts
+++ workspace/front-end/angular-app/src/app/tmb-ecert/sup00000/sup01000/sup01000.component.ts
@@ -53,7 +53,7 @@ export class Sup01000Component implements OnInit {
 
   userRoleForm: FormGroup = new FormGroup({
     roleName: new FormControl('', Validators.required),
-    status: new FormControl(90001, Validators.required)
+    status: new FormControl(90002, Validators.required)
   });
 
   uploadForm: FormGroup = new FormGroup({
@@ -173,7 +173,7 @@ export class Sup01000Component implements OnInit {
 
 
     clickClear() {
-      this.userRoleForm.reset({ roleName: "", status: 0 });
+      this.userRoleForm.reset({ roleName: "", status: 90002 });
       this.store.dispatch(new SUP01000ACTION.ClearRole());
       this.statusDropDown.clear();
       this.roleDT.clear();
